94e5fddb6cefa534877840160683f34e83bc5760
Kamil Mastalerz <kamil.mastalerz@netguru.com>
2021-02-22 17:18:29 +0100

    Add config for AWS Amplify

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_ROeChtzP3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1em8n5km1im20rn3u8rif6hu5q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
