81afe0287b93ff54dd9ad506e2625ebac1ccd238
Kamil Mastalerz <kamil.mastalerz@netguru.com>
2021-02-22 17:55:34 +0100

    Add static website files

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-central-1_ROeChtzP3', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1em8n5km1im20rn3u8rif6hu5q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-central-1' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
